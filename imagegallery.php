343d3ddce8bd5d31305a0a77fa4d7e1a3b735345
Logan Duncan <l.duncan2@students.clark.edu>
2020-05-05 12:17:32 -0700

    Added Normalize

--- imagegallery.php
+++ imagegallery.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styling/stye.css">
     <link rel="stylesheet" href="styling/style.css">
     <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
     <title>Image Gallery</title>
