746e4651527ea655eccd7b8f9681089528aabc83
Logan Duncan <l.duncan2@students.clark.edu>
2020-05-17 13:19:26 -0700

    Removed

--- helper/is_logged_in.php
+++
@@ -1,8 +0,0 @@
-<?php
-// is_logged_in.php
-session_start();
-if (isset($_SESSION['username'])) {
-    echo json_encode(["status" => 'yes']);
-} else {
-    echo json_encode(["status" => 'no']);
-}
--- helper/logout_ajax.php
+++
@@ -1,5 +0,0 @@
-<?php
-// logout_ajax.php
-session_start();
-session_destroy();
-echo json_encode(['status' => 'success']);
